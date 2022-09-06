9652705da049d18ec63672a5dc47de68c33069e2
BRUNO <ava426@einstein.br>
2022-09-06 14:46:28 -0300

    Atualização AXIOS 06091450

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,9 +1,11 @@
 import { ParamListBase, NavigationProp, useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from '../../assets/logo.svg';
+import { api } from '../../services/api';
+import { CarDTO } from '../../dtos/CarDTO';
 
 import { Car } from '../../Components/Car';
 
@@ -16,6 +18,8 @@ import {
 } from './styles';
 
 export function Home(){
+  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [loading, setLoading] =  useState(true);
   const carData = {
     brand: 'Audi',
     name: 'RS 5 Coupe',
@@ -32,15 +36,21 @@ export function Home(){
     navigation.navigate('CarDetails');
   } 
 
-  // const carDataTwo = {
-  //   brand: 'Porsche',
-  //   name: 'Panamera',
-  //   rent: {
-  //     period: 'Ao dia',
-  //     price: 340,
-  //   },
-  //   thumbnail: 'https://purepng.com/public/uploads/large/purepng.com-porsche-panamera-white-carcarvehicletransportporsche-961524659258pfvb8.png'
-  // } 
+  useEffect(() => {
+    async function fetchCars() {
+      try {
+        const response = await api.get('/cars');
+        setCars(response.data);
+
+      } catch (error) {
+        console.log(error);
+      }finally {
+        setLoading(false);
+      }
+    }
+
+    fetchCars();
+  }, []);
 
   return (
     <Container>
@@ -61,9 +71,9 @@ export function Home(){
       </Header>
 
       <CarList 
-        data={[1,2,3,4,5,6,7]}
-        keyExtractor={item => String(item)}
-        renderItem={({ item }) => <Car data={carData} onPress={handleCarDetails}/>}
+        data={cars}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <Car data={item} onPress={handleCarDetails}/>}
       />
    
     </Container>
